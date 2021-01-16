3f69e012f5be30f75b4ff10788f5572631c52311
Daniel Kiss <kissd621@gmail.com>
2021-01-16 07:45:51 +0100

    Added hidden fields middleware to protect certain fields

--- src/loaders/koa.ts
+++ src/loaders/koa.ts
@@ -5,27 +5,19 @@ import helmet from 'koa-helmet'
 
 import { loggerMiddleware } from '@logger'
 import router from '@components/router'
-import Logger from '@logger'
+import errorHandler from '@api/middleware/error-handler'
+import hiddenFields from '@api/middleware/hidden-fields'
 
 export default (app: Koa<Koa.DefaultState, Koa.DefaultContext>) => {
-  app.use(helmet()).use(bodyParser()).use(cors()).use(loggerMiddleware)
-
-  // ERROR HANDLING
-  app.use(async (ctx, next) => {
-    try {
-      await next()
-    } catch (err) {
-      const status = err.status || 500
-      ctx.body = {
-        status: err.title || ctx.status >= 500 ? 'error' : 'fail',
-        message: err.message,
-      }
-      ctx.status = status
-
-      Logger.error('Request failed: %s', err.message)
-    }
-  })
-
-  // Load routes
-  app.use(router().routes()).use(router().allowedMethods())
+  app
+    // Global middleware
+    .use(helmet())
+    .use(bodyParser())
+    .use(cors())
+    .use(loggerMiddleware)
+    .use(errorHandler)
+    .use(hiddenFields)
+    // Load routes
+    .use(router().routes())
+    .use(router().allowedMethods())
 }
