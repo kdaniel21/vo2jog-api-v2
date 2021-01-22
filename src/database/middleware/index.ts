4ed92bbc50d3767ad04fcdcae69a7471b56ce154
Daniel Kiss <kissd621@gmail.com>
2021-01-22 17:41:14 +0100

    Delete prisma related files

--- src/database/middleware/index.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client'
-import encryptPassword from '@database/middleware/encrypt-password'
-
-// Register all middleware
-export default (prismaClient: PrismaClient) => {
-  encryptPassword(prismaClient)
-}
--- src/database/prisma/migrations/20210117142112_init3/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "events" ALTER COLUMN "is_deleted" SET DEFAULT false;
